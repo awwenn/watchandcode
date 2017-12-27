244f7d9741bc50dea02a72f8b7bf7714dd90914c
Allen Carstensen <allencarstensen@gmail.com>
2017-12-27 09:58:26 -0700

    js document, most information pulled from version-6, but with added document grabbers to make use of two new variables - Display Todos, and Toggle All.

---
+++ version-7.js
@@ -0,0 +1,75 @@
+let todoList = {
+  todos: [],
+  displayTodos: function() {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty.');
+    } else {
+      console.log('My Todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }
+    }
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    let todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+    // get number of completed todos and change the completedTodos variable to that number
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+
+    // case 1: if everything's true, make everything false
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+
+    // case 2: otherwise, make everything true
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
+  }
+};
+
+// 1. we want to get access to the display todos button
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+// 2. we want to run the display todos method when someone clicks
+// the display todos button
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+})
+
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();
+})
